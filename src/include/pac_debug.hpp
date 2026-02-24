8f95dbb73cc01e3936d1a13d421ab1155d50c057
ila <ilaria.battiston@gmail.com>
2026-02-24 10:13:41 +0100

    debug prints

--- src/include/pac_debug.hpp
+++ src/include/pac_debug.hpp
@@ -2,7 +2,7 @@
 
 // Set to 1 to enable verbose PAC debug output, 0 to disable
 // This is separate from DuckDB's DEBUG macro to avoid cluttering test output
-#define PAC_DEBUG 1
+#define PAC_DEBUG 0
 
 #if PAC_DEBUG
 #define PAC_DEBUG_PRINT(x) Printer::Print(x)
