ff957673ec627efc9b789eae347328c99268699d
peter <boncz@cwi.nl>
2026-03-14 01:22:32 +0100

    fix bugs in hand-written queries

--- benchmark/tpch/tpch_pac_queries/q05.sql
+++ benchmark/tpch/tpch_pac_queries/q05.sql
@@ -2,7 +2,7 @@ SELECT n_name, pac_noised_sum(pac_hash(hash(c_custkey)), l_extendedprice * (1 - 
   FROM customer JOIN orders ON c_custkey = o_custkey 
                 JOIN lineitem ON o_orderkey = l_orderkey 
                 JOIN supplier ON l_suppkey = s_suppkey 
-                JOIN nation ON s_nationkey = c_nationkey 
+                JOIN nation ON s_nationkey = n_nationkey AND c_nationkey = s_nationkey
                 JOIN region ON n_regionkey = r_regionkey
  WHERE r_name = 'ASIA' AND o_orderdate >= DATE '1994-01-01' AND o_orderdate <  DATE '1995-01-01'
  GROUP BY ALL
--- benchmark/tpch/tpch_pac_queries/q22.sql
+++ benchmark/tpch/tpch_pac_queries/q22.sql
@@ -10,7 +10,7 @@ FROM (SELECT substring(c_phone FROM 1 FOR 2) AS cntrycode, c_acctbal,
                                AND substring(c_phone FROM 1 FOR 2) IN ('13', '31', '23', '29', '30', '18', '17'))) AS pac_pu,
         FROM customer
         WHERE substring(c_phone FROM 1 FOR 2) IN ('13', '31', '23', '29', '30', '18', '17')
-         AND pac_filter(pac_pu)
+         AND pac_pu <> 0
          AND NOT EXISTS (FROM orders WHERE o_custkey = customer.c_custkey)) AS custsale
  GROUP BY ALL
  ORDER BY ALL;
